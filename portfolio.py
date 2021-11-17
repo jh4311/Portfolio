601e1f15617926e2f74cf1920a6f1c3c4a5da3ae
jh4311 <94498259+jh4311@users.noreply.github.com>
2021-11-16 21:05:38 -0500

    implement test

--- portfolio.py
+++ portfolio.py
@@ -1,6 +1,6 @@
 class Portfolio:
-    def __init__(self, stocks):
-        self.stocks = []
+    def __init__(self):
+        self._stocks = []
     
     def buy(self, name, shares, price):
         self._stocks.append((name, shares, price))
