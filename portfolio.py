2f071dcfadd79bb4e0c55a2ddd00f147c030d11c
jh4311 <94498259+jh4311@users.noreply.github.com>
2021-11-16 20:16:25 -0500

    Create portfolio.py

---
+++ portfolio.py
@@ -0,0 +1,11 @@
+class Portfolio:
+    def __init__(self):
+        self.stocks = []
+    
+    def buy(self, name, shares, price):
+        self._stocks.append((name, shares, price))
+
+    def cost(self):
+        return sum(
+            shares * price for name, shares, price in self._stocks
+        )
