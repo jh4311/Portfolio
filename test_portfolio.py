682e3d37a94161e00e05977bcaf3a472d7201b0c
jh4311 <94498259+jh4311@users.noreply.github.com>
2021-11-16 20:58:17 -0500

    implement test

---
+++ test_portfolio.py
@@ -0,0 +1,12 @@
+from portfolio import Portfolio
+
+def best_buy_one_stock():
+    p = Portfolio()
+    p.buy("IBM", 100, 176.48)
+    assert p.cost() == 17648.0
+
+def test_buy_two)stocks()p = Portfolio()
+    p = Portfolio()
+    p.buy("IBM", 100, 176.48)
+    p.buy("HPQ", 100, 36.15)
+    assert p.cost() == 21263.0
